08444aa68e7cbb73ffebe3bcfbbedf59d6b6405d
𝑫𝑹𝑨𝑲𝑬𝑵|☤|𝐄𝐫𝐠𝐞𝐧𝐞𝐤𝐨𝐧 <167642673+drakensama1@users.noreply.github.com>
2024-05-07 23:05:06 +0300

    Update kana.js

--- plugins/kana.js
+++ plugins/kana.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
   const chat = global.db.data.chats[m.chat];
   if (/^دراكن$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*اهلا, ¿انا لوفي?*`);
+    await m.reply(`*اهلا, ¿انا دراكن?*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
   return !0;
